7906e6b7f4375ec6d87707ee3bd070adfa70dd45
Denis <denisglb@yahoo.co.uk>
2017-08-14 14:35:59 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+ "Hello Jenny!!!
+/\n
+ rejgenq	w;rw 	wkjqfbf	ewqwgf v unicorn"
+end
+
+get '/cat' do
+  
+"<div> 
+<img src='http://bit.ly/1eze8aE' style='border: medium dashed red'>
+
+</div>"
+
+end
+
